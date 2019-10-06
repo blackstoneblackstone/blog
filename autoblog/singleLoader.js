4fbd47f2db1d9e91ca7f2a95ce4f11cc6a231798
Shadow <601735416@qq.com>
2019-10-06 12:33:50 +0800

    5536b36a38d52e827065eb249d2de343
    5536b36a38d52e827065eb249d2de343

--- autoblog/singleLoader.js
+++ autoblog/singleLoader.js
@@ -2,4 +2,4 @@
  * Created by lihongbin on 2018/11/5.
  */
 const {shareLoad} = require('./shareLoader')
-shareLoad("bf425fe49ab4933f78173872d6b8a3fd")
+shareLoad("5536b36a38d52e827065eb249d2de343")
